1e627a5e3beaaa42ff5d7ddcde1012d482576482
GayThatClownman <60339604+GayThatClownman@users.noreply.github.com>
2020-03-23 09:42:16 +0100

    Brainstorm + Test av Switch cases
    Jag har tänkt mycket kring projektet, samtidigt som jag har testat att använda mig av switch cases som ett alternativ till else if-statements, med mindre lyckade resultat.

--- Slutprojekt_K5/Program.cs
+++ Slutprojekt_K5/Program.cs
@@ -13,19 +13,49 @@ namespace Slutprojekt_K5
             //Notes: Text-Based Dungeon Äventyr-kinda thing.
             
             int hp = 100;
+            string gameState = "";
+
+            /*int stateSwitch = 1;
+
+            switch (stateSwitch)
+            {
+                case 1:
+                    gameState = "Gamer";
+                    break;
+                case 2:
+                    gameState = "Non-Gamer";
+                    break;
+            }
+
+            while (gameState == "Gamer")
+            {
+                Console.WriteLine("Gamer");
+                stateSwitch++;
+                Console.ReadLine();
+            }
+
+            while (gameState == "Non-Gamer")
+            {
+                Console.WriteLine("Non-Gamer");
+                stateSwitch--;
+                Console.ReadLine();
+            }*/
 
             /*List<string> weaponsList = new List<string>();*/
 
-            string[] weaponsList =
+            /*string[] weaponsList =
             {
-                "", "", "", "", "",
-            };
+                "Wooden Shortsword", "", "", "", "",
+            };*/
 
-            bool checkGameCompletion = true;
+            bool checkGameCompletion = false;
 
             while (checkGameCompletion == false)
             {
+                if (gameState == "intro")
+                {
 
+                }
             }
 
             if (checkGameCompletion == true)
