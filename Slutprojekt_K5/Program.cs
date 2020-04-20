ee2d3928cedde2733adc2c341c872f910b9a0e2f
GayThatClownman <60339604+GayThatClownman@users.noreply.github.com>
2020-04-20 09:14:42 +0200

    Implementering av kommentarer
    Kommentarer har implementerats på de ett antal platser.

--- Slutprojekt_K5/Program.cs
+++ Slutprojekt_K5/Program.cs
@@ -14,19 +14,23 @@ namespace Slutprojekt_K5
         {
             //Notes: Stage-based Fight Spel med progressive scaling..
             
+            //Olika variabler för spelar-hp och damagevärden. Gamestate är en kvarleva från tidigare tankar till projektet, som inte har mycket använding något mer.
             int hp = 100;
             string gameState = "gameStart";
             string action = "";
             int damage = 0;
             int enemyDamage = 0;
 
+            //Variabel som kollar vilken nivå som man har nått, som *skulle* styra flera aspekter av spelet.
             int stage = 1;
 
+            //Int-Array som ska motsvara livet på fienden beroende på level.
             int[] enemyHealth =
             {
                 10, 25, 50, 75, 100, 150,
             };
 
+            //String-array av vapen som skulle vara tillgängliga i shopen, som ens Damage-värden skulle bestämmas utifrån
             string[] weaponsList =
             {
                 "Wooden Shortsword", "1", "2", "3", "4",
@@ -34,29 +38,24 @@ namespace Slutprojekt_K5
 
             string currentWeapon = weaponsList[0];
 
+            //Bool som kollar om spelet är avklarat eller inte. Tänkt för att kunna ge spelaren en End-Screen
             bool checkGameCompletion = false;
 
             while (checkGameCompletion == false)
             {
-                /*if (gameState == "intro")
-                {
-                    Intro();
-                    
-                    gameState = "gameStart";                    
-                }*/
                 
-
                 while (gameState == "gameStart")
                 {
 
                     if (stage == 1 && enemyHealth[0] > 0)
                     {
-                        
+                        //Skriver ut livet av fiende, spelarens alternativ och tar emot spelar val.
                         Console.WriteLine("Current enemy health: " + enemyHealth[0]);
                         FightPrompt();
                         //Navigate(action);
                         action = Console.ReadLine().ToLower();
 
+                        //Om spelaren valde alternativ A så genereras ett tal som skada som fienden tar. Efter det gör detsamma från fiendens sida. Detta repeteras tills fienden är har förlorat all HP.
                         if (action == "a")
                         {
                             damage = DamageGenerator(1, 5);
@@ -70,6 +69,7 @@ namespace Slutprojekt_K5
 
                         }
 
+                        //Om spelaren valde alternativ C så avslutas programmet.
                         if (action == "c")
                         {
                             Environment.Exit(0);
@@ -78,17 +78,10 @@ namespace Slutprojekt_K5
 
                     else
                     {
-                        Console.WriteLine("Bruh tf");
+                        Console.WriteLine("Funkar inte.");
                     }
                 }
-
-                /*if (gameState == "test")
-                {
-                    //damage = DamageGenerator(125, 200);
-                    //Console.WriteLine(damage);
-                    Console.WriteLine("OkDog");
-                    Console.ReadLine();
-                }*/                
+           
             }
 
             if (checkGameCompletion == true)
@@ -101,7 +94,7 @@ namespace Slutprojekt_K5
 
             
         }
-
+        //Metod som skriver ut de olika valen man har under en fight.
         static void FightPrompt()
         {
             string[] choices =
@@ -128,6 +121,7 @@ namespace Slutprojekt_K5
             return action;
         }*/
 
+        //Metod som genererar ett tal utifrån två variabler.
         static int DamageGenerator(int dmg1, int dmg2)
         {
             Random dmgGenerator = new Random();
